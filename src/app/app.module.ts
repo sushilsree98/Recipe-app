124f213504789073541bc70fecd2bb9e5c687ab1
sushilsree98 <sushil.sree98@gmail.com>
2020-09-15 11:37:03 +0530

    Recipe feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,32 +6,23 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeComponent } from './recipe/recipe.component';
-import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropDownDirective } from './shared/dropdown.directive'
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipe/recipe.service';
 import { AuthComponent } from './auth/auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component';
+import { RecipeModule } from './recipe/recipe.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent,
-    RecipeComponent, RecipeListComponent,
-    RecipeDetailComponent, RecipeItemComponent,
+    AppComponent, HeaderComponent,  
     ShoppingListComponent, ShoppingEditComponent,
     DropDownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent
@@ -40,6 +31,7 @@ import { AlertComponent } from './shared/alert/alert.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    RecipeModule,
     ReactiveFormsModule,
     HttpClientModule
   ],
@@ -48,6 +40,7 @@ import { AlertComponent } from './shared/alert/alert.component';
     useClass: AuthInterceptorService,
     multi:true
   }],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[AppRoutingModule]
 })
 export class AppModule { }
