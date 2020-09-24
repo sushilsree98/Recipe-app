0d6c5df52385e245246a4de3255336b02cae5844
sushilsree98 <sushil.sree98@gmail.com>
2020-09-24 21:04:14 +0530

    module routing configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RecipeComponent } from './recipe/recipe.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
-import { RecipeResolverService } from './recipe/recipe.resolver.service';
 import { AuthComponent } from './auth/auth/auth.component';
-import { AuthGuard } from './auth/auth.guard';
 
 
 const routes: Routes = [
@@ -15,33 +9,6 @@ const routes: Routes = [
     path:'',
     redirectTo:"/recipe",
     pathMatch:'full'
-  },
-
-  {
-    path:"recipe",
-    component:RecipeComponent,
-    canActivate:[AuthGuard],
-    children:[
-      {
-        path:"",
-        component:RecipeStartComponent,
-      },
-      {
-        path:'new',
-        component:RecipeEditComponent
-      },
-      {
-        path:":id",
-        component:RecipeDetailComponent,
-        resolve: [RecipeResolverService]
-      },
-      {
-        path:':id/edit',
-        component:RecipeEditComponent,
-        resolve:[RecipeResolverService]
-      },
-     
-    ]
   },
   {
     path:"shoppinglist",
--- src/app/recipe/recipe.module.ts
+++ src/app/recipe/recipe.module.ts
@@ -6,6 +6,7 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeRoutingModule } from './recipe-routing.module';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeComponent } from './recipe.component';
 
@@ -13,7 +14,7 @@ import { RecipeComponent } from './recipe.component';
     declarations: [RecipeComponent, RecipeListComponent,
         RecipeDetailComponent, RecipeItemComponent, RecipeStartComponent,
         RecipeEditComponent],
-    imports: [RouterModule, CommonModule, ReactiveFormsModule],
+    imports: [RouterModule, CommonModule, ReactiveFormsModule, RecipeRoutingModule],
     exports:[
         RecipeComponent, RecipeListComponent,
         RecipeDetailComponent, RecipeItemComponent, RecipeStartComponent,
